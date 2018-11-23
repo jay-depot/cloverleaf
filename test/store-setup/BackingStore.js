d1604ea796e406bf9b867c657350ccf721565849
Jason DePeaux <jason@thefarmersdog.com>
2018-11-23 17:24:31 -0500

    Missed a few in BackingStore

--- test/store-setup/BackingStore.js
+++ test/store-setup/BackingStore.js
@@ -2,7 +2,10 @@ describe('cloverleaf.BackingStore', () => {
   it('should throw if constructed directly');
   context('when extended by another class', () => {
     it('should throw if select method is not defined by child class');
+    it('should throw if property select is not a function in child class');
     it('should throw if commit method is not defined by child class');
+    it('should throw if property commit is not a function in child class');
     it('should throw if discard method is not defined by child class');
+    it('should throw if property discard is not a function in child class');
   });
 });
