d46f05c0c12a4905a175b60177e71853f70be65e
Jason DePeaux <jason@thefarmersdog.com>
2018-11-23 23:08:39 -0500

    store.getSnapshotFromSelector tests and implementation

--- test/state-snapshot/getStateSnapshotBySelector.js
+++ test/state-snapshot/getStateSnapshotBySelector.js
@@ -1,31 +1,93 @@
-describe('store.createStateSnapshotBySelector()', () => {
-  context('called with undefined as a selector', () => {
-    it('throws an exception');
+const cloverleaf = require('../../index');
+const sinon = require('sinon');
+
+describe('store.getStateSnapshotBySelector()', () => {
+  context('when called without a selector', () => {
+    it('rejects with an error', () => {
+      class DummyBackingStore extends cloverleaf.BackingStore {
+        commit() {}
+        discard() {}
+        select() {}
+      }
+      const store = cloverleaf.createStore(new DummyBackingStore());
+
+      return store.getStateSnapshotBySelector().should.be.rejected();
+    });
   });
 
-  context('called with empty object as a selector', () => {
-    it('passes the selector to the backing store');
-    specify('state snapshot object must have property named items');
-    specify('items must match the results from the backing store');
-    specify(
-      'state snapshot object must have method named dispatchToStateSnapshot'
-    );
-    specify('state snapshot object must have method named commitStateSnapshot');
-    specify(
-      'state snapshot object must have method named discardStateSnapshot'
-    );
-  });
+  context('when called with a selector', () => {
+    it('passes the selector to the backing store', async () => {
+      const mockBackingStore = {
+        commit() {},
+        discard() {},
+        select: sinon.spy(),
+      };
+      const store = cloverleaf.createStore(mockBackingStore);
+      await store.getStateSnapshotBySelector({ test: 'selector' });
+      mockBackingStore.select.should.be.calledWith({ test: 'selector' });
+    });
 
-  context('called with non-empty object as a selector', () => {
-    it('passes the selector to the backing store');
-    specify('state snapshot object must have property named items');
-    specify('items must match the results from the backing store');
     specify(
-      'state snapshot object must have method named dispatchToStateSnapshot'
+      'state snapshot object must have property named items',
+      async () => {
+        const mockBackingStore = {
+          commit() {},
+          discard() {},
+          select() {},
+        };
+        const store = cloverleaf.createStore(mockBackingStore);
+        const selector = await store.getStateSnapshotBySelector({
+          test: 'selector',
+        });
+
+        selector.should.have.property('items');
+      }
     );
-    specify('state snapshot object must have method named commitStateSnapshot');
-    specify(
-      'state snapshot object must have method named discardStateSnapshot'
+
+    specify('items must match the results from the backing store', async () => {
+      const mockBackingStore = {
+        commit() {},
+        discard() {},
+        select: async () => [
+          { id: 1, test: 'selector' },
+          { id: 2, test: 'selector' },
+          { id: 3, test: 'selector' },
+        ],
+      };
+      const store = cloverleaf.createStore(mockBackingStore);
+      const selector = await store.getStateSnapshotBySelector({
+        test: 'selector',
+      });
+
+      selector.items.should.deepEqual([
+        { id: 1, test: 'selector' },
+        { id: 2, test: 'selector' },
+        { id: 3, test: 'selector' },
+      ]);
+    });
+
+    [
+      'dispatchToStateSnapshot',
+      'commitStateSnapshot',
+      'discardStateSnapshot',
+    ].forEach(requiredMethod =>
+      specify(
+        `resulting object must have method named ${requiredMethod}`,
+        async () => {
+          const mockBackingStore = {
+            commit() {},
+            discard() {},
+            select: sinon.spy(),
+          };
+          const store = cloverleaf.createStore(mockBackingStore);
+          const snapshot = await store.getStateSnapshotBySelector({
+            test: 'selector',
+          });
+
+          snapshot.should.have.property(requiredMethod);
+          snapshot[requiredMethod].should.be.a.Function();
+        }
+      )
     );
   });
 });
