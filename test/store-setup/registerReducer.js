332b559385b567b601ab00a01675684d1e023f4f
Jason DePeaux <jason@thefarmersdog.com>
2018-11-23 21:57:31 -0500

    implement registerReducer

--- test/store-setup/registerReducer.js
+++ test/store-setup/registerReducer.js
@@ -1,4 +1,22 @@
+const cloverleaf = require('../../index');
+const should = require('should');
+
 describe('store.registerReducer()', () => {
-  it('should return the store with reducer attached');
-  it('should throw if called more than once');
+  class DummyBackingStore extends cloverleaf.BackingStore {
+    commit() {}
+    discard() {}
+    select() {}
+  }
+
+  it('should return the store with reducer attached', () => {
+    const store = cloverleaf.createStore(new DummyBackingStore());
+    store.registerReducer(() => {});
+    store.should.have.property('reducer');
+    store.reducer.should.be.a.Function();
+  });
+  it('should throw if called more than once', () => {
+    const store = cloverleaf.createStore(new DummyBackingStore());
+    should.doesNotThrow(() => store.registerReducer(() => {}));
+    should.throws(() => store.registerReducer(() => {}));
+  });
 });
